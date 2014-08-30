c27b1f7ab1def71b8690e5391bd90fedfa459e7d
Lorie Ransom <lorie@ransomdesign.com>
2014-08-29 18:47:03 -0700

    added a dummy filter to the thumbnail view. Need to work on logic to map to user clicks

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,8 @@
 // Declare app level module which depends on filters, and services
 var myEmag = angular.module('myEmag', [
   'ngRoute',
-  'emagControllers'
+  'emagControllers',
+  'emagFilters'
 ]);
 
 myEmag.config(['$routeProvider', function($routeProvider) {
@@ -28,22 +29,57 @@ emagControllers.controller('ThumbnailCtrl', ['$scope', '$http', function($scope,
       $scope.pages = data;
     });
     $scope.limitVal = 5;
+    // I want the number of thumbs shown to include 2 before and 2 after the one clicked, so a total of 5
+    // The thumbails shown will update as soon as the user clicks on one.
+    // The array math:
+    // pages objects = 12.
+    // if the user click xvalue, resultArray =  pages.splice(xvalue-2, 5)
+    // how do I bookend for the end of the array?
+
     $scope.path = 'page_01.jpg';
     $scope.clickThumb = function(value) {
      	$scope.activePage = value;
       $scope.path = $scope.pages[value].image.toString();
-            console.log($scope.path);
-    };  
-
-  
-
+            // console.log($scope.path);
+    };
+    // function updatePageRange($scope){
+    //   var clickedPage = $scope.activePage;
+    //   var }
 
     // $scope.bigPage = function () {
     //   angular.forEach(pages, function(value, key) {
     //     console.log('for each called');
     //   });
-    
 }]);
+
+
+//Filters 
+
+var emagFilters = angular.module('emagFilters',[]);
+
+emagFilters.filter('filterThumbs', function()
+{
+  return function (items, showSmallSet)
+  {
+        var range = 4;
+        var filtered = [];
+        for (var i = 0; i < items.length ; i++) 
+        {
+            var item = items[i];
+            for(var j = 0; j < range; j++)
+            {
+                if(item.page_num == j )
+                {
+                   filtered.push(item);
+                }
+            }
+        }
+            console.log(filtered);
+     return filtered;
+  };
+});
+
+
 /*!
  * IE10 viewport hack for Surface/desktop Windows 8 bug
  * Copyright 2014 Twitter, Inc.
