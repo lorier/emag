b71846239cfaae81897c4d0d2c4363ac134f24be
Lorie Ransom <lorie@ransomdesign.com>
2014-09-01 12:01:00 -0700

    Resized images and added pagination code

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
 var myEmag = angular.module('myEmag', [
   'ngRoute',
@@ -10,14 +9,12 @@ var myEmag = angular.module('myEmag', [
 
 myEmag.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
-      // when('/prev', {
-      //   templateUrl:'partials/page2.html', 
-      // }).
-  		when('/', {
-  		  templateUrl:'partials/page1.html' ,
+  		when('/view/', {
+  		templateUrl:'partials/page1.html' ,
   		}).
-      //something's fucky with this function. It was breaking routprovider when I listed the prev button route after it. 
-  		when('/:pageNum', {
+      //something's fucky with this function. It was breaking routprovider when I listed the prev button route after it.
+      //only alternative I know of is a verbose listing of each html page
+  		when('/view/:pageNum', {
         templateUrl: function(params){ return 'partials/page' + params.pageNum + '.html'; },
   		}).
      
@@ -28,67 +25,65 @@ myEmag.config(['$routeProvider', function($routeProvider) {
 
 var emagControllers = angular.module ('emagControllers', []);
 
-emagControllers.controller('ThumbnailCtrl', ['$scope', '$http', '$location',function($scope, $http, $location){
-	//get the json data
+emagControllers.controller('ThumbnailCtrl', ['$scope', '$http', '$location', '$route', '$routeParams', function($scope, $http, $location, $route, $routeParams){
+   //initialize variables
+  $scope.pages = {};
+  $scope.activePage = 1;
+
+  //get the json data
   $http.get('json/magazine.json').success(function(data) {
       $scope.pages = data;
     });
+  
+  $scope.applyBorder = function(value){
+    if(value == $scope.activePage){
+      return 'true';
+    }
+  };
 
-    $scope.activePage = 1;
-    // get the page_num value through the click event.
-    $scope.clickThumb = function(value) {
-      //assign the page_num to the var activePage;
-     	$scope.activePage = value;
+  //Logic for page buttons
+  $scope.clickThumb = function(value) {
+    //assign the page_num to the var activePage;
+    
+    //****This is the state****//
+   	$scope.activePage = value;
+    //*************************//
+  };
 
-//attempting to code for prev and next
-      };
-      $scope.clickPrev = function(){
-
+  //Logic for prev/next buttons. Could use some refactoring
+  $scope.advanceSinglePage = function(direction){
+    if(direction == 'prev'){
       var prevPage = $scope.activePage-1;
-      $scope.activePage = prevPage;
+      if (prevPage > 0 ){
+        $scope.activePage = prevPage;
+      }
+    }else if(direction == 'next'){
+      var nextPage = $scope.activePage+1;
+      if (nextPage <= $scope.pages.length ){
+        $scope.activePage = nextPage;
+      }
+    }
+    //concat the new url provided by the function logic
+    //and feed it into the $location service. This updates the route.
+    var loc = '/view/' + $scope.activePage.toString();
+    $location.path(loc);
+  };
 
-      var loc = '/' + prevPage.toString();
-      console.log( $location.path(loc));
-      $location.path(loc);
-
-      };
-
-     
+    //use $routeParams to load the current URL - based on p592 in Pro AngularJS
+    //-$routParams comes back empty. -apparently this will only work if 
+    //the controller to which it is attached isn inside ng-view. Ditching this for now.
+    // $scope.$on("$routeChangeSuccess", function(){
+    //   if($location.path().indexOf("/view") == 0) {
+    //     var id = $routeParams["page_num"];
+    //     console.log('id: ' + angular.toJson($routeParams);
+    //     for (var i = 0; i < $scope.pages.length; i++){
+    //       if ($scope.pages[i].page_num == id){
+    //         $scope.activePage = $scope.pages[i];
+    //         break;
+    //       }
+    //     } 
+    //   }    
 }]);
-
-//Attempting to provide a variable to the templateURL in myEmag.config based on the active page.
-// !! need to somehow access the scope of the emagController.not sure this is the solution 
-// var emagProviders = angular.module ('emagProviders', []);
-// emagProviders.provider('URLConfig', function() {
-//     return 'partials/page1.html';
-// });   
-
-
-
-//Filters 
-//attempted to limit the number of pagination tiles via filter, but this altered the model
-// var emagFilters = angular.module('emagFilters',[]);
-// emagFilters.filter('filterThumbs', function()
-// {
-//   return function (items, showSmallSet)
-//   {
-//         var range = 4;
-//         var filtered = [];
-//         for (var i = 0; i < items.length ; i++) 
-//         {
-//             var item = items[i];
-//             for(var j = 0; j < range; j++)
-//             {
-//                 if(item.page_num == j )
-//                 {
-//                    filtered.push(item);
-//                 }
-//             }
-//         }
-//             console.log(filtered);
-//      return filtered;
-//   };
-// });
 
 
 /*!
@@ -100,7 +95,6 @@ emagControllers.controller('ThumbnailCtrl', ['$scope', '$http', '$location',func
 
 // See the Getting Started docs for more information:
 // http://getbootstrap.com/getting-started/#support-ie10-width
-
 (function () {
   'use strict';
   if (navigator.userAgent.match(/IEMobile\/10\.0/)) {
