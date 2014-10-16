06f7b08f38bbb97c73067c56471f5960fee3d565
Lorie Ransom <lorie@ransomdesign.com>
2014-10-16 11:51:09 -0700

    Updates to app.js
    Refactored the “state” into a factory provider, started to hard-code
    the routes.

--- app/js/app-old.js
+++ app/js/app-old.js
@@ -1,54 +1,22 @@
 'use strict';
-/*--------------------------
-  My Local URL: http://localhost6:8888/angular-seed-emag/app/#/view/4
-  --------------------------*/
 
 // Declare app level module which depends on filters, and services
 var myEmag = angular.module('myEmag', [
   'ngRoute',
   'ngAnimate',
-  'emagControllers',
-  'emagFactories'
+  'emagControllers'
 ]);
-
-// Adding a factory to keep track of state. Code courtesy of
-// http://onehungrymind.com/building-a-website-with-angularjs-pt-2-now-with-state/
-// https://www.youtube.com/watch?v=WEGppmA9URM#t=26
-// 
-var emagFactories = angular.module ('emagFactories', []);
-
-emagFactories.factory('StateService', function(){
-  var activePage = 1;
-  var getActivePage = function(){
-    return activePage;
-  };
-  var setActivePage = function(pg){
-    activePage = pg;
-    console.log(activePage);
-  };
-  return {
-      getActivePage: getActivePage,
-      setActivePage: setActivePage
-    };
-    
-  });
 
 myEmag.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
   		when('/view/', {
   		templateUrl:'partials/page1.html' ,
   		}).
-      when('/view/2', {
-      templateUrl:'partials/page2.html' ,
-      }).
-      when('/view/3', {
-      templateUrl:'partials/page13.html' ,
-      }).
       //something's fucky with this function. It was breaking routprovider when I listed the prev button route after it.
       //only alternative I know of is a verbose listing of each html page
-  		// when('/view/:pageNum', {
-    //     templateUrl: function(params){ return 'partials/page' + params.pageNum + '.html';} 
-  		// }).
+  		when('/view/:pageNum', {
+        templateUrl: function(params){ return 'partials/page' + params.pageNum + '.html'; },
+  		}).
      
  	otherwise({redirectTo: '/view'});
 }]);
@@ -57,60 +25,65 @@ myEmag.config(['$routeProvider', function($routeProvider) {
 
 var emagControllers = angular.module ('emagControllers', []);
 
-emagControllers.controller('ThumbnailCtrl', ['$scope', 'StateService', '$http', '$location', '$route', '$routeParams', function($scope, StateService, $http, $location, $route, $routeParams){
+emagControllers.controller('ThumbnailCtrl', ['$scope', '$http', '$location', '$route', '$routeParams', function($scope, $http, $location, $route, $routeParams){
    //initialize variables
   $scope.pages = {};
+  $scope.activePage = 1;
 
   //get the json data
   $http.get('json/magazine.json').success(function(data) {
       $scope.pages = data;
     });
-  $scope.activePage = StateService.getActivePage;
-
-  $scope.updateActivePage = function(value){
-      StateService.setActivePage(value);
-  };
+  
   $scope.applyBorder = function(value){
-    if(value == $scope.activePage()){
+    if(value == $scope.activePage){
       return 'true';
     }
   };
-  $scope.showThumb = function(value){
-    if ((value >= $scope.activePage() - 1) && (value <= $scope.activePage() + 1)) {
-      return true;
-    }
-  };
+
   //Logic for page buttons
   $scope.clickThumb = function(value) {
     //assign the page_num to the var activePage;
+    
     //****This is the state****//
-   	//$scope.activePage = value;
+   	$scope.activePage = value;
     //*************************//
-    $scope.updateActivePage(value);
   };
 
   //Logic for prev/next buttons. Could use some refactoring
   $scope.advanceSinglePage = function(direction){
     if(direction == 'prev'){
-      var prevPage = $scope.activePage()-1;
+      var prevPage = $scope.activePage-1;
       if (prevPage > 0 ){
-        $scope.updateActivePage(prevPage);
+        $scope.activePage = prevPage;
       }
     }else if(direction == 'next'){
-      var nextPage = $scope.activePage()+1;
+      var nextPage = $scope.activePage+1;
       if (nextPage <= $scope.pages.length ){
-        $scope.updateActivePage(nextPage);
+        $scope.activePage = nextPage;
       }
     }
     //concat the new url provided by the function logic
     //and feed it into the $location service. This updates the route.
-    var loc = '/view/' + $scope.activePage().toString();
-    console.log(loc);
+    var loc = '/view/' + $scope.activePage.toString();
     $location.path(loc);
-  };    
+  };
+
+    //use $routeParams to load the current URL - based on p592 in Pro AngularJS
+    //-$routParams comes back empty. -apparently this will only work if 
+    //the controller to which it is attached isn inside ng-view. Ditching this for now.
+    // $scope.$on("$routeChangeSuccess", function(){
+    //   if($location.path().indexOf("/view") == 0) {
+    //     var id = $routeParams["page_num"];
+    //     console.log('id: ' + angular.toJson($routeParams);
+    //     for (var i = 0; i < $scope.pages.length; i++){
+    //       if ($scope.pages[i].page_num == id){
+    //         $scope.activePage = $scope.pages[i];
+    //         break;
+    //       }
+    //     } 
+    //   }    
 }]);
-
-
 
 
 /*!
